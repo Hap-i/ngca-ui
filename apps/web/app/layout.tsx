6fdfb73f61b82f77b556b1fd9c02795475fdbf3f
suvendu <suvendusekharsahoo5@gmail.com>
2026-05-15 02:16:44 +0530

    theme update

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,15 +1,20 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
-
+import { Inter, JetBrains_Mono } from "next/font/google"
+import type { Metadata } from "next";
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@workspace/ui/lib/utils";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'})
 
-const fontMono = Geist_Mono({
+const fontMono = JetBrains_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
-})
+});
+
+export const metadata: Metadata = {
+  title: "Next Gen Cricket Academy",
+  description: "Next Gen Cricket Academy is a premier cricket training facility dedicated to nurturing and developing the next generation of cricketing talent. Our academy offers world-class coaching, state-of-the-art facilities, and a comprehensive training program designed to help aspiring cricketers reach their full potential. Whether you're a beginner looking to learn the basics or an experienced player aiming to refine your skills, Next Gen Cricket Academy provides the perfect environment for growth and success in the sport of cricket.",
+};
 
 export default function RootLayout({
   children,
